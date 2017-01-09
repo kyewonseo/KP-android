98710130d643048f8e9dff95ad981a7415d8b090
kyewon <k@bluehack.net>
2017-01-09 22:28:50 +0900

    [KIOSK-11] gitignore , Application class, Log Manager 추가

--- app/src/main/java/net/bluehack/kiosk/ApplicationLoader.java
+++ app/src/main/java/net/bluehack/kiosk/ApplicationLoader.java
@@ -1,7 +1,26 @@
 package net.bluehack.kiosk;
 
 import android.app.Application;
+import android.content.Context;
+
+import static net.bluehack.kiosk.util.Logger.makeLogTag;
 
 public class ApplicationLoader extends Application {
+
+    private static String TAG = makeLogTag(ApplicationLoader.class);
+
+    private static Context instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+
+
+    }
+
+    public static Context getContext() {
+        return instance.getApplicationContext();
+    }
 
 }
