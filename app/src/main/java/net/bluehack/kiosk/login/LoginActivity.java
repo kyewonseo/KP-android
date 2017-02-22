f7cd52954512ae58a5008dd02b7815d6c6140c1c
kyewon <k@bluehack.net>
2017-02-22 14:47:25 +0900

    [KIOSK-11] add login layout

--- app/src/main/java/net/bluehack/kiosk/login/LoginActivity.java
+++ app/src/main/java/net/bluehack/kiosk/login/LoginActivity.java
@@ -1,4 +1,19 @@
 package net.bluehack.kiosk.login;
 
-public class LoginActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+import net.bluehack.kiosk.R;
+
+import static net.bluehack.kiosk.util.Logger.makeLogTag;
+
+public class LoginActivity extends Activity {
+
+    private static final String TAG = makeLogTag(LoginActivity.class);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
 }
